16da8e52fa8dc11bb6a7126ad7b6f0a42ace6e5b
dr6z <131973867+dr6z@users.noreply.github.com>
2026-09-12 17:03:52 +0200

    Create Class1.cs

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections.Generic;
+
+namespace rentcar
+{
+    public static class CarDatabase
+    {
+        public static List<Car> Cars { get; } = new List<Car>();
+    }
+}
